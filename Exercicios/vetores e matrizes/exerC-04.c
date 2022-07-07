d8b2c2394b4ecd31a842a660ebd772c3a35aa39b
Leticia <leticiazattera949@gmail.com>
2022-07-07 09:29:30 -0300

    Alterações iniciais

--- Exercicios/vetores e matrizes/exerC-04.c
+++
@@ -1,9 +0,0 @@
-/*4. Fac¸a um programa que leia um vetor de 8 posic¸oes e, em seguida, leia tamb ˜ em dois va- ´
-lores X e Y quaisquer correspondentes a duas posic¸oes no vetor. Ao final seu programa ˜
-devera escrever a soma dos valores encontrados nas respectivas posic¸ ´ oes ˜ X e Y*/
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
-}
--- agenda/f_agenda.h
+++
@@ -1,4 +0,0 @@
-void *inserir(void *pBuffer);
-void buscar(void *pBuffer);
-void listar(void *pBuffer);
-void *apagar(void *pBuffer);
